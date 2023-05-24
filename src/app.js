4d136ea121c0232f15211ae5399c0636a5a5a01a
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-05-24 13:24:32 +0330

    add socket io

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 const express = require("express");
+
+const http = require("http");
+const socketio = require("socket.io");
 const bodyParser = require("body-parser");
+const path = require("path");
 require("dotenv").config();
 
 require("./db/mongo");
 
+const publicDir = path.join(__dirname, "../public");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(publicDir));
 
-module.exports = app;
+io.on("connection", (socket) => {
+  console.log("New websocket connection!");
+});
+
+module.exports = server;
